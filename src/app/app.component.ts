eba13af798e36ac08ec7bc89ae3d797054fd6430
jujully <julia.ramos@iclass.com.br>
2025-10-15 15:15:52 -0300

    feat: adiciona property binding e event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'secao5-projeto';
+  inputText = 'Valor Inicial';
+  inputType = 'text';
+  isDisabled = false;
+
+
+  enableInput() {
+    this.isDisabled = false;
+  }
+  disableInput() {
+    this.isDisabled = true;
+  }
+  setPasswordInput() {
+    this.inputType = 'password';
+  }
+  setTextInput() {
+    this.inputType = 'text';
+  }
+  logInputText() {
+    console.log(this.inputText);
+  }
+  handleInputKeyup(event: KeyboardEvent) {
+    const currentText = event.target as HTMLInputElement;
+    console.log(currentText.value);
+  }
+  handleInputEvent(event: Event) {
+    const currentText = event.target as HTMLInputElement;
+    console.log(currentText.value);
+  }
+
 }
+
